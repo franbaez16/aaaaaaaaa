ae5bab1fa00fc03e8ef002ec47228162005b4a7e
Fran Baez <103434629+franbaez16@users.noreply.github.com>
2022-09-05 15:42:49 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,16 @@
+function logearse(){
+    var x,y;
+    x = document.getElementById("usuario").value; 
+    y = document.getElementById("contra").value;
+    var usuario="eddie34"
+    var contra="eddie.forever"
+
+if (usuario == "x" && contra== "y")
+{
+    alert ("Su usuario y contraseña son correctos")
+}
+else
+{
+    alert ("Su usuario o contraseña son incorrectos. Pruebe nuevamente")
+}
+}
